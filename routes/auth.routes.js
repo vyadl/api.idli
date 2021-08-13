7941dbaec48126beba79c7e60fcf06a51a2d445c
Philip <hipafi@gmail.com>
2021-08-13 20:15:15 +0300

    test cors

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,13 +3,13 @@ const { verifySignUp, validation } = require('./../middlewares');
 const controller = require('../controllers/auth.controller');
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, ContentType, Accept',
-    );
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'x-access-token, Origin, ContentType, Accept',
+  //   );
+  //   next();
+  // });
 
   app.post(
     '/api/auth/signup',
