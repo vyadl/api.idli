e1751e4cdc35b48f609fbb7a2e68b86c12a90bd2
Philip <hipafi@gmail.com>
2021-08-14 15:36:16 +0300

    test cors

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ const createApp = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    next();
+  });
+
   app.get('/', (req, res) => {
     res.json({ message: 'Welcome to idli application' })
   });
