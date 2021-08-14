88b27c0f0a1965e5a9266060f3056c57c34d4cba
Philip <hipafi@gmail.com>
2021-08-14 15:45:25 +0300

    test cors

--- server.js
+++ server.js
@@ -9,14 +9,23 @@ const createApp = async () => {
 
   const app = express();
 
-  const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+  // ## CORS middleware
+  // 
+  // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+  var allowCrossDomain = function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        
+      // intercept OPTIONS method
+      if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
   };
-
-  app.use(cors(corsOptions));
+  app.use(allowCrossDomain);
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
